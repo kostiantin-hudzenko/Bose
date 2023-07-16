0fd8257faa6935f8fe9542915a0295f37e5045c0
Kostiantyn Hudzenko <119158387+kostiantin-hudzenko@users.noreply.github.com>
2023-07-16 14:44:02 +0300

    Add files via upload

---
+++ index.d65fe41c.js
@@ -0,0 +1,2 @@
+"use strict";const form=document.querySelector("form");window.addEventListener("hashchange",(()=>{"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")})),form.addEventListener("submit",(e=>{e.preventDefault(),e.target.reset()}));
+//# sourceMappingURL=index.d65fe41c.js.map
